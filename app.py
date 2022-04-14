d9d3f28eb3774c220c66abc673ca6a8f88bc8664
tanvinaminul <tanvinaminul@gmail.com>
2022-04-14 17:48:08 +0600

    Add Japanese to English

--- app.py
+++ app.py
@@ -8,8 +8,11 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/', methods=['POST'])
-def index_post():
+@app.route('/en', methods=['GET'])
+def index_en():
+    return render_template('translate.html')
+@app.route('/en', methods=['POST'])
+def en_post():
     # Read the values from the form
     text = flask_request.form['text']
     
@@ -57,4 +60,60 @@ def index_post():
         response=response,
         text=text
 
-    )   
+    )
+@app.route('/ja', methods=['GET'])
+def index_ja():
+    return render_template('translate.html')
+@app.route('/ja', methods=['POST'])
+def post_ja():
+    # Read the values from the form
+    text = flask_request.form['text']
+    
+    # Load the values from .env
+    key = "56021f41d65c4246bdf51a20569d0cee"
+    endpoint = "https://api.cognitive.microsofttranslator.com/"
+    location = "eastus"
+
+
+    # Indicate that we want to translate and the API version (3.0) and the target language
+    path = '/translate'
+    constructed_url = endpoint + path
+
+    params = {
+    'api-version': '3.0',
+    'from': 'ja',
+    'to': ['en']
+    }
+    #params[to]=flask_request.form('language')
+    # Set up the header information, which includes our subscription key
+    headers = {
+        'Ocp-Apim-Subscription-Key': key,
+        'Ocp-Apim-Subscription-Region': location,
+        'Content-type': 'application/json',
+        'X-ClientTraceId': str(uuid.uuid4())
+    }
+
+
+    body = [{
+        'text': text
+    }]
+    # Make the call using post
+    request = requests.post(constructed_url, params=params, headers=headers, json=body)
+    # Retrieve the JSON response
+    response = request.json()
+
+    # Retrieve the translation
+    response = response[0]['translations'][0]['text']
+
+
+    # Call render template, passing the translated text,
+    # # original text, and target language to the template
+    return render_template(
+        'results.html',
+        response=response,
+        text=text
+
+    )
+   
+if __name__== "__main__":
+    app.run(debug=False, host= '0.0.0.0')
